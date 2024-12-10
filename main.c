8102ce6356adcd4187242fe2e98f71e3af21ac26
youcef <abdelhakim1cef@gmail.com>
2024-12-10 19:05:15 +0100

    table d'allocation

--- main.c
+++ main.c
@@ -1,12 +1,11 @@
-#include<stdio.h>
-#include<string.h>
-#include<stdbool.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <stdbool.h>
 
 // ************************************************************
 // ************************************************************
 
-#define MaxBlocks 30;
+#define MaxBlocks 30
 
 typedef struct {
     int id;
@@ -16,10 +15,10 @@ typedef struct {
 
 typedef struct {
     enregistrement tab[3];
-    int address; 
+    int address;
     int nbEnregistrement;
     bool isFree;
-}block, buffer;
+} block, buffer;
 
 typedef struct {
     char fileName[40];
@@ -27,9 +26,9 @@ typedef struct {
     int sizeBlocks;
     int sizeEnrgs;
     int addressFirst;
-    bool modeOrganizationGlobal; //chaine / contegu;
-    bool modeOrganizationIntern; // trier / !trier;
-}metaData;
+    bool modeOrganizationGlobal; //chaine / contegu
+    bool modeOrganizationIntern; // trier / !trier
+} metaData;
 
 typedef struct {
     int fileId;
@@ -37,23 +36,68 @@ typedef struct {
 
 typedef struct {
     int address;
-    bool free // true for free and false for !true;
-}BlockState;
+    bool free; // true for free and false for not free
+} BlockState;
 
-BlockState Ms[MaxBlocks]
+char fileName[] = "Ms.dat";
 
 // ***********************************************************************
 // ***********************************************************************
 
-void initialisationDisque(FILE *ms) {
-    
-    for(int i = 0; i < MaxBlocks; i ++) {
+void initialisationDisque(char fileName[]) {
+    BlockState Ms[MaxBlocks];
+    FILE *ms = fopen(fileName, "wb");
+
+    if (ms == NULL) {
+        perror("Error opening file for writing");
+        return;
+    }
+
+    for (int i = 0; i < MaxBlocks; i++) {
         Ms[i].address = i;
         Ms[i].free = true;
     }
 
-    size_t fwrite(Ms, sizeof(BlockState), MaxBlocks, ms);
+    size_t elements = fwrite(Ms, sizeof(BlockState), MaxBlocks, ms);
+    if (elements != MaxBlocks) {
+        perror("Error writing to file");
+    } else {
+        printf("Blocks State Table written successfully\n");
+    }
 
+    fclose(ms);
+}
+
+void tableDallocation(char operation, int add) {
+    BlockState Ms[MaxBlocks];
+    BlockState state;
+    FILE *ms = fopen(fileName, "rb+");
+
+    if (ms == NULL) {
+        perror("Error opening file for reading/writing");
+        return;
+    }
+
+    fseek(ms, sizeof(BlockState) * add, SEEK_SET);
+    state.address = add;
+
+    switch (operation) {
+        case 'f': 
+            state.free = true;
+        break;
+        
+        case 'a': 
+            state.free = false;
+        break;
+        
+        default:
+            printf("Invalid operation\n");
+            fclose(ms);
+        return;
+    }
+
+    fwrite(&state, sizeof(BlockState), 1, ms);
+    fclose(ms);
 }
 
 int main() {
