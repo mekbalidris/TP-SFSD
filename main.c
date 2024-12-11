4f17b84599d2a32b6ab484c60ba5b0d34a707827
youcef <abdelhakim1cef@gmail.com>
2024-12-11 10:30:18 +0100

    somme updates in the structs

--- main.c
+++ main.c
@@ -26,8 +26,8 @@ typedef struct {
     int sizeBlocks;
     int sizeEnrgs;
     int addressFirst;
-    bool modeOrganizationGlobal; //chaine / contegu
-    bool modeOrganizationIntern; // trier / !trier
+    bool modeOrganizationGlobal;
+    bool modeOrganizationIntern;
 } metaData;
 
 typedef struct {
@@ -36,17 +36,18 @@ typedef struct {
 
 typedef struct {
     int address;
-    bool free; // true for free and false for not free
+    bool free;
 } BlockState;
 
-char fileName[] = "Ms.dat";
+typedef struct {
+    metaData filesArray[MaxBlocks];
+    int numberOf_files;
+} FilesMeta;
 
-// ***********************************************************************
-// ***********************************************************************
-// ****************initialization ********************
 void initialisationDisque(char fileName[]) {
     BlockState Ms[MaxBlocks];
     buffer buf;
+    FilesMeta metas;
     FILE *ms = fopen(fileName, "wb");
 
     if (ms == NULL) {
@@ -68,6 +69,9 @@ void initialisationDisque(char fileName[]) {
 
     printf("Blocks State Table written successfully\n");
 
+    metas.numberOf_files = 0;
+    fwrite(&metas, sizeof(FilesMeta), 1, ms);
+
     for (int i = 0; i < MaxBlocks; i++) {
         memset(&buf, 0, sizeof(buf));
         buf.address = i;
@@ -85,13 +89,7 @@ void initialisationDisque(char fileName[]) {
     fclose(ms);
 }
 
-
-// ***************************************************************
-// ***************************************************************
-// *****************tableDallocation *****************************
-
-
-void tableDallocation(char operation, int add) {
+void tableDallocation(char fileName[], char operation, int add) {
     BlockState Ms[MaxBlocks];
     BlockState state;
     FILE *ms = fopen(fileName, "rb+");
@@ -107,112 +105,43 @@ void tableDallocation(char operation, int add) {
     switch (operation) {
         case 'f': 
             state.free = true;
-        break;
+            break;
 
         case 'a': 
             state.free = false;
-        break;
+            break;
         
         default:
             printf("Invalid operation\n");
             fclose(ms);
-        return;
+            return;
     }
 
     fwrite(&state, sizeof(BlockState), 1, ms);
     fclose(ms);
 }
-// ************************************************************
-// ************************************************************
-// ************* compactage:
 
-void compactage(char fileName[]){
-    FILE *ms = fopen(fileName, "r+b");
-    if(ms == NULL){
-        perror("error opening file");
-        return;
-    }
+void compactage(char fileName[]) {
+    block msBlocks[MaxBlocks];
+    FILE* ms = fopen(fileName, "r+b");
+    block msBlocks[MaxBlocks];
 
-    BlockState blockStates[MaxBlocks];
-    buffer buff, compactedBuffer;
+    fseek(ms, sizeof(BlockState) * MaxBlocks + sizeof(FilesMeta), SEEK_SET);
 
-    fseek(ms, 0, SEEK_SET);
-    fread(blockStates, sizeof(BlockState), MaxBlocks, ms);
+    fread(msBlocks, sizeof(block), MaxBlocks, ms);
 
-    compactedBuffer.address = 0;
-    compactedBuffer.nbEnregistrement = 0;
-    compactedBuffer.isFree = false;
-
-    int compactedBlockIndex = 0;
-
-    for (int i = 0; i < MaxBlocks; i++) {
-        if (!blockStates[i].free) {
-            // Read the current block
-            fseek(ms, sizeof(BlockState) + i * sizeof(buffer), SEEK_SET);
-            fread(&buff, sizeof(buffer), 1, ms);
-
-            // Traverse records in the block and move non-deleted ones
-            for (int j = 0; j < buff.nbEnregistrement; j++) {
-                if (!buff.tab[j].isDeleted) {
-                    compactedBuffer.tab[compactedBuffer.nbEnregistrement] = buff.tab[j];
-                    compactedBuffer.nbEnregistrement++;
-
-                    // If the compacted buffer is full, write it and reset
-                    if (compactedBuffer.nbEnregistrement == 3) {
-                        fseek(ms, sizeof(BlockState) + compactedBlockIndex * sizeof(buffer), SEEK_SET);
-                        fwrite(&compactedBuffer, sizeof(buffer), 1, ms);
-
-                        compactedBlockIndex++;
-                        memset(&compactedBuffer, 0, sizeof(buffer));
-                        compactedBuffer.address = compactedBlockIndex;
-                        compactedBuffer.nbEnregistrement = 0;
-                        compactedBuffer.isFree = false;
-                    }
-                }
-            }
-        }
-    }
-
-    //write the remaining recors in compacted buffer if any
-
-    if(compactedBuffer.nbEnregistrement > 0){
-        fseek(ms, sizeof(BlockState) + compactedBlockIndex * sizeof(buffer), SEEK_SET);
-        fwrite(&compactedBuffer, sizeof(buffer), 1, ms);
-        compactedBlockIndex++;
-    }
-
-     // Update block states: mark unused blocks as free
-    for (int i = compactedBlockIndex; i < MaxBlocks; i++) {
-        blockStates[i].free = true;
-        fseek(ms, sizeof(BlockState) + i * sizeof(buffer), SEEK_SET);
-        memset(&buff, 0, sizeof(buffer));
-        buff.address = i;
-        buff.nbEnregistrement = 0;
-        buff.isFree = true;
-        fwrite(&buff, sizeof(buffer), 1, ms);
-    }
-
-    // Update block state table
-    fseek(ms, 0, SEEK_SET);
-    fwrite(blockStates, sizeof(BlockState), MaxBlocks, ms);
 
     fclose(ms);
-    printf("Disk compacted successfully.\n");
 }
-
-
-
-// ************************************************************
-// ************************************************************
-// *************vidange de la memoire centrale ***************
 
 void vider(char fileName[]) {
     BlockState tableDallocation[MaxBlocks];
     buffer buf;
+    FilesMeta metas;
     FILE *ms = fopen(fileName, "r+b");
 
     if (ms == NULL) {
-        perror("Error opening file ");
+        perror("Error opening file");
         return;
     }
 
@@ -223,6 +152,10 @@ void vider(char fileName[]) {
 
     fseek(ms, 0, SEEK_SET);
     fwrite(tableDallocation, sizeof(BlockState), MaxBlocks, ms);
+    
+    metas.numberOf_files = 0;
+    fwrite(&metas, sizeof(FilesMeta), 1, ms);
+
 
     for (int i = 0; i < MaxBlocks; i++) {
         memset(&buf, 0, sizeof(buffer));
