03a471d703168f3e2be67e3cc4e1ecdfd20d69df
mekbalidris <mekbalidriss@gmail.com>
2024-12-11 02:20:11 +0100

    added comptage function

--- main.c
+++ main.c
@@ -125,6 +125,83 @@ void tableDallocation(char operation, int add) {
 // ************************************************************
 // ************************************************************
 // ************* compactage:
+
+void compactage(char fileName[]){
+    FILE *ms = fopen(fileName, "r+b");
+    if(ms == NULL){
+        perror("error opening file");
+        return;
+    }
+
+    BlockState blockStates[MaxBlocks];
+    buffer buff, compactedBuffer;
+
+    fseek(ms, 0, SEEK_SET);
+    fread(blockStates, sizeof(BlockState), MaxBlocks, ms);
+
+    compactedBuffer.address = 0;
+    compactedBuffer.nbEnregistrement = 0;
+    compactedBuffer.isFree = false;
+
+    int compactedBlockIndex = 0;
+
+    for (int i = 0; i < MaxBlocks; i++) {
+        if (!blockStates[i].free) {
+            // Read the current block
+            fseek(ms, sizeof(BlockState) + i * sizeof(buffer), SEEK_SET);
+            fread(&buff, sizeof(buffer), 1, ms);
+
+            // Traverse records in the block and move non-deleted ones
+            for (int j = 0; j < buff.nbEnregistrement; j++) {
+                if (!buff.tab[j].isDeleted) {
+                    compactedBuffer.tab[compactedBuffer.nbEnregistrement] = buff.tab[j];
+                    compactedBuffer.nbEnregistrement++;
+
+                    // If the compacted buffer is full, write it and reset
+                    if (compactedBuffer.nbEnregistrement == 3) {
+                        fseek(ms, sizeof(BlockState) + compactedBlockIndex * sizeof(buffer), SEEK_SET);
+                        fwrite(&compactedBuffer, sizeof(buffer), 1, ms);
+
+                        compactedBlockIndex++;
+                        memset(&compactedBuffer, 0, sizeof(buffer));
+                        compactedBuffer.address = compactedBlockIndex;
+                        compactedBuffer.nbEnregistrement = 0;
+                        compactedBuffer.isFree = false;
+                    }
+                }
+            }
+        }
+    }
+
+    //write the remaining recors in compacted buffer if any
+
+    if(compactedBuffer.nbEnregistrement > 0){
+        fseek(ms, sizeof(BlockState) + compactedBlockIndex * sizeof(buffer), SEEK_SET);
+        fwrite(&compactedBuffer, sizeof(buffer), 1, ms);
+        compactedBlockIndex++;
+    }
+
+     // Update block states: mark unused blocks as free
+    for (int i = compactedBlockIndex; i < MaxBlocks; i++) {
+        blockStates[i].free = true;
+        fseek(ms, sizeof(BlockState) + i * sizeof(buffer), SEEK_SET);
+        memset(&buff, 0, sizeof(buffer));
+        buff.address = i;
+        buff.nbEnregistrement = 0;
+        buff.isFree = true;
+        fwrite(&buff, sizeof(buffer), 1, ms);
+    }
+
+    // Update block state table
+    fseek(ms, 0, SEEK_SET);
+    fwrite(blockStates, sizeof(BlockState), MaxBlocks, ms);
+
+    fclose(ms);
+    printf("Disk compacted successfully.\n");
+}
+
+
+
 // ************************************************************
 // ************************************************************
 // *************vidange de la memoire centrale ***************
@@ -159,6 +236,10 @@ void vider(char fileName[]) {
 
     fclose(ms);
 }
+
+//***************************FILE MANAGMENT************************************
+
+// Function to create a new data file
 
 int main() {
 
